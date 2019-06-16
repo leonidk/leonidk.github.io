f90e246df7f1f21aefa37e8bfb048cdf00cd2285
leonidk <lkeselman@gmail.com>
2019-06-16 18:22:19 -0400

    better favicon scripts

--- _make_favicon.sh
+++ _make_favicon.sh
@@ -1,1 +1,1 @@
-convert -background transparent "_favicon.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+convert -background transparent "images/images/circle_bw_crop.jpg" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
